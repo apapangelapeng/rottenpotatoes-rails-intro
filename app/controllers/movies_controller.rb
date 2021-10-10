dd382804ddac73688675de0af6cf701131eb44d7
Angela Peng <apapangelapeng@gmail.com>
2021-10-10 23:56:14 +0000

    added more stuff

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,14 +39,7 @@ class MoviesController < ApplicationController
     # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
     #  movies with those ratings
     # if ratings_list is nil, retrieve ALL movies
-      if ratings_list == nil:
-         @movies = Movie.all
-      else:
-        for r in ratings_list do 
-          @movie = Movie.where("rating='r'")
-        end
-      end
-      
+      Movie.where(rating:rating_list)
     end
      
    def self.all_ratings ; %w[G PG PG-13 R NC-17] ; end 
