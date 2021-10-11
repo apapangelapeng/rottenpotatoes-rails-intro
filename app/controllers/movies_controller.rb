f02837dfccf85c5642dc17ff46a23dc5d5352282
Angela Peng <apapangelapeng@gmail.com>
2021-10-11 01:51:13 +0000

    added code in the index page to retrive dictionary and sorted page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,10 +7,21 @@ class MoviesController < ApplicationController
   end
 
   def index
+    checked_box = params[:ratings]
+    sorted = params[:sort]||session[:sort]
+    if check_boxes.nil:
+      @ratings_to_show = []
+    else:
+      @ratings_to_show = check_boxes.keys
+    end
+
     if check_boxes.nil?:
       @movies = Movie.all
     else:
       @movies = Movie.with_ratings(@ratings_to_show)
+    end
+    @movies = movies.order(sorted)
+    @all_ratings = Movie.all_ratings
   end
 
   def new
@@ -34,15 +45,16 @@ class MoviesController < ApplicationController
     redirect_to movie_path(@movie)
   end
 
-  class Movie
+  class Movie < ActiveRecord::Base
+    @all_rating = ["G","PG","PG-13","R","NC-17"]
     def self.with_ratings(ratings_list)
-    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
-    #  movies with those ratings
-    # if ratings_list is nil, retrieve ALL movies
       Movie.where(rating:rating_list)
     end
      
-   def self.all_ratings ; %w[G PG PG-13 R NC-17] ; end 
+   def self.all_ratings 
+     @all_ratings
+   
+   end 
  end
   
   def destroy
